489dd46a20326d2dc79e48abd7e94585e9f40aa1
noceive <nocivescience@gmail.com>
2023-04-03 05:28:09 -0400

    la nave se orienta en el espacio

--- script.js
+++ script.js
@@ -27,21 +27,57 @@ const text_size= 40;
 var ship, level, roids, x, y;
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
-// setInterval(update, 1000);
-ship=newShip();
-level=1;
-createAsteroidsBelt();
-update();
+newGame();
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
+setInterval(update, 10);
 function newShip(){
     return {
         x: canvas.width/2,
         y: canvas.height/2,
         r: ship_size/2,
+        rot:0,
         a: 90/180*Math.PI,
         blinkNum: Math.ceil(ship_inv_dur/ship_blink_dur),
         explodeTime: 0,
+        thrusting: false,
+        thrust: {x:0, y:0},
     }
 }
+function keyDown(/** @type {KeyboardEvent} */ ev){
+    if (ship.dead){
+        return;
+    }
+    switch(ev.key){
+        case "p":
+            break;
+        case "a":
+            ship.rot= ship_turn_spd/180*Math.PI/fps;
+            break;
+        case "d":
+            ship.rot= -ship_turn_spd/180*Math.PI/fps;
+            break;
+        case "w":
+            ship.thrusting= true;
+            break;
+    }
+}
+function keyUp(/** @type {KeyboardEvent} */ ev){
+    if (ship.dead){
+        return;
+    }
+    switch(ev.key){
+        case "a":
+            ship.rot= 0;
+            break;
+        case "d":
+            ship.rot= 0;
+            break;
+        case "w":
+            ship.thrusting= false;
+            break;
+    }
+};
 function distBetweenPoints(x1, y1, x2, y2){
     return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
 }
@@ -73,11 +109,37 @@ function newAsteroid(x,y,r){
         roid.offs.push(Math.random()*roid_jag*2+1-roid_jag);
     }
 };
+function newGame(){
+    ship=newShip();
+}
+function drawShip(x,y,a, colour="white"){
+    ctx.strokeStyle=colour;
+    ctx.lineWidth=ship_size/20;
+    ctx.beginPath();
+    ctx.moveTo(
+        x+4/3*ship.r*Math.cos(a),
+        y-4/3*ship.r*Math.sin(a)
+    );
+    ctx.lineTo(
+        x-ship.r*(2/3*Math.cos(a)+Math.sin(a)),
+        y+ship.r*(2/3*Math.sin(a)-Math.cos(a))
+    );
+    ctx.lineTo(
+        x-ship.r*(2/3*Math.cos(a)-Math.sin(a)),
+        y+ship.r*(2/3*Math.sin(a)+Math.cos(a))
+    );
+    ctx.closePath();
+    ctx.stroke();
+}
 function update(){
     var blinkOn=ship.blinkNum%2==0;
     var exploding=ship.explodeTime>0;
     ctx.filsStyle="black";
     ctx.fillRect(0,0,canvas.width,canvas.height);
-    var a,r,x,y, offset, vert;
-    console.log(roids);
-}   
+    ship.a+=ship.rot/10;
+    if (ship.thrusting){
+        ship.x+=ship_thrust*Math.cos(ship.a)/fps*10;
+        ship.y-=ship_thrust*Math.sin(ship.a)/fps*10;
+    }
+    drawShip(ship.x, ship.y, ship.a);
+}
